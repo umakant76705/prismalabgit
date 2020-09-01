a6925096ba0d2778f77e15fdcdb612dad88c6731
umakant76705 <31900360+umakant76705@users.noreply.github.com>
2020-09-01 15:34:31 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.gamutkart$i container.."
+    echo "Creating www.prismalabtest$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it nageshvkn/gamutkart-img /bin/bash
-    echo "www.gamutkart$i container has been created!"
+    docker run --name www.prismalabtest$i -d -it umakant76705/kantprisma-img /bin/bash
+    echo "www.prismalabtest$i container has been created!"
 	echo "=============================="
 done
 docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
